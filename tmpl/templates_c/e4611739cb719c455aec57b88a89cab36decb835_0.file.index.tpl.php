39167c832914ac387c71469e3d1540b64f748d9c
Daniscr99 <danuele.sciarretta@gmail.com>
2021-07-16 16:20:15 +0200

    Update e4611739cb719c455aec57b88a89cab36decb835_0.file.index.tpl.php

--- tmpl/templates_c/e4611739cb719c455aec57b88a89cab36decb835_0.file.index.tpl.php
+++ tmpl/templates_c/e4611739cb719c455aec57b88a89cab36decb835_0.file.index.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-07-16 16:08:21
+/* Smarty version 3.1.39, created on 2021-07-16 16:19:37
   from 'C:\Users\danue\public_html\Prog_Web_Cos\tmpl\templates\index.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_60f192d53edd72_16103827',
+  'unifunc' => 'content_60f195790335c8_15191646',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'e4611739cb719c455aec57b88a89cab36decb835' => 
     array (
       0 => 'C:\\Users\\danue\\public_html\\Prog_Web_Cos\\tmpl\\templates\\index.tpl',
-      1 => 1626444471,
+      1 => 1626445172,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_60f192d53edd72_16103827 (Smarty_Internal_Template $_smarty_tpl) {
+function content_60f195790335c8_15191646 (Smarty_Internal_Template $_smarty_tpl) {
 ?><html>
 <head>
  <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -51,7 +51,7 @@ function content_60f192d53edd72_16103827 (Smarty_Internal_Template $_smarty_tpl)
 					
 			    <ul class="navbar-nav mr-auto" id="links">
                                         <li class="nav-item">
-                                            <a class="navbar-brand" href="#"><img src="img/logo def.png" width="50px" heigth="20px" ></a>
+                                            <a class="navbar-brand"><img src="img/Senza titolo-1.png" width="80px" heigth="25px" ></a>
 					</li>
 					<li class="nav-item">
 						<a class="nav-link" href="#">Home</a>
