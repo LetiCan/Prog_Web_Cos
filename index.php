79d3c7a628b290f3fb717de2a48e343104521ec2
Daniscr99 <danuele.sciarretta@gmail.com>
2021-08-12 12:14:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
  <?php 
- require_once 'impo/configura.inc';
- require 'View/View.php';
- $v=new View();
+require_once 'Foundation/Gestione/Gpreleva.php';
+require 'Controllo/Chome.php';
+$cix = Gpreleva::getIstanza('Chome');
+$cix->home();
+/* $v=new View();
  $v->caricaindex('index.tpl');
- /**<?php include 'filehtml\header.html'?>
+ <?php include 'filehtml\header.html'?>
     <br><br>
     <?php include 'filehtml\home.html'?>
     <br><br>
     <?php include 'filehtml\footer.html'?>	
- **/
+ */
