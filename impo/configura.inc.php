48a863d1e9a019a387496f407fc3361cdcd95998
Daniscr99 <danuele.sciarretta@gmail.com>
2021-08-12 11:32:29 +0200

    Create configura.inc.php

---
+++ impo/configura.inc.php
@@ -0,0 +1,9 @@
+<?php
+global $configura;
+
+$configura['mysql']['host']='localhost';
+$configura['mysql']['user']='pippo';
+$configura['mysql']['pass']='123';
+$configura['mysql']['dbname']='test';
+
+?>
