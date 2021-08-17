b3bd238c8cafd39acb9f7e8bde886ef54a6fddcd
Daniscr99 <danuele.sciarretta@gmail.com>
2021-08-17 18:26:48 +0200

    Update Clogin.php

--- Controllo/Clogin.php
+++ Controllo/Clogin.php
@@ -3,10 +3,12 @@ require_once 'Foundation/Gestione/Gpreleva.php';
 require_once 'Foundation/Gestione/Gsessione.php';
 require_once 'Foundation/Futente.php';
 require_once 'Foundation/Fconndb.php';
+require_once 'View/VUtente.php';
 class Clogin
 {
     private $username;
     private $password;
+    private $messaggio;
     
     public function autenticazione($u,$p)
     {
@@ -21,9 +23,25 @@ class Clogin
         }
         else
         {
-            echo 'credenziali sbagliate';
+            $this->messaggio= 'credenziali sbagliate';
         }
         
+    }
+
+    public function InsUtente()
+    {
+        $vu=Gpreleva::getIstanza('VUtente');
+        $dreg=$vu->DatiReg();
+        $Fu= new Futente();
+        $ut=$Fu->preleva_user($dreg['username'],$dreg['pwd']);
+        if(iseet($ut))
+        {
+            $this->messaggio='Utente Esistente';
+        }
+        else
+        {
+            $Fu->InserisciDati($dreg);
+        }
     }
 }
 ?>
