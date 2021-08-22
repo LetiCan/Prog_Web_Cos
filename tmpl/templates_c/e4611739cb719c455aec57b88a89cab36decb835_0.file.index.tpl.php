c82264d1c531b6dbb3c6534f605b7f8298aba693
Daniscr99 <danuele.sciarretta@gmail.com>
2021-08-22 11:28:43 +0200

    Update e4611739cb719c455aec57b88a89cab36decb835_0.file.index.tpl.php

--- tmpl/templates_c/e4611739cb719c455aec57b88a89cab36decb835_0.file.index.tpl.php
+++ tmpl/templates_c/e4611739cb719c455aec57b88a89cab36decb835_0.file.index.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-08-18 15:32:41
+/* Smarty version 3.1.39, created on 2021-08-22 10:57:35
   from 'C:\Users\danue\public_html\Prog_Web_Cos\tmpl\templates\index.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_611d0bf9633df2_94852649',
+  'unifunc' => 'content_6122117f0b8493_82082439',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'e4611739cb719c455aec57b88a89cab36decb835' => 
     array (
       0 => 'C:\\Users\\danue\\public_html\\Prog_Web_Cos\\tmpl\\templates\\index.tpl',
-      1 => 1629217601,
+      1 => 1629622649,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_611d0bf9633df2_94852649 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6122117f0b8493_82082439 (Smarty_Internal_Template $_smarty_tpl) {
 ?><html>
 <head>
  <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -110,7 +110,7 @@ function content_611d0bf9633df2_94852649 (Smarty_Internal_Template $_smarty_tpl)
 					<label for="password"><b>Password</b></label>
 					<input type="password" placeholder="Password"  id="psw" name="psw" >
 
-					<input type="submit" onclick="checkLogin();">Accedi</input>
+					<input type="submit" value="Accedi" onclick="checkLogin();"></input>
 				</div>		
 			</form>
 		</div>
