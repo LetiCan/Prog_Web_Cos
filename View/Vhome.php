fab3e4fe859e6d85b8b8d1827b562b4c6121a936
Daniscr99 <danuele.sciarretta@gmail.com>
2021-08-27 16:55:19 +0200

    Update Vhome.php

--- View/Vhome.php
+++ View/Vhome.php
@@ -2,11 +2,36 @@
 require_once 'View.php';
 class Vhome extends View
 {
-    private $layout;
+    private $layout_reg;
 
-    public function home()
+    public function Registrazione()
     {
-       return $this->layout->caricaindex('index.tpl');
+      $this->layout_reg=$this->showTmp('Registrazione.tpl');
+      if(isset($this->layout_reg))  
+      {
+          return true;
+      }
+      else{
+          return false;
+      }
+     
+    }
+    public function DatiReg()
+    {
+        if(isset($_POST['nome'],$_POST['cognome'],$_POST['datanascita'],$_POST['ldn'],$_POST['cdf'],$_POST['sesso'],$_POST['username'],$_POST['pwd']))
+        {
+            $d=array($_POST['nome'],$_POST['cognome'],$_POST['datanascita'],$_POST['ldn'],$_POST['cdf'],$_POST['sesso'],$_POST['username'],$_POST['pwd']);
+         /* foreach($d as $dto)
+            {
+                if(isset($_POST[$dto]))
+                {
+                    $a[$dto]=$_POST[$dto];
+                }
+            }*/
+            
+        }
+        return $d;
+        
     }
 }
 ?>
