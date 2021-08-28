ced1f6c7351cd7129909e55da97b930fe82f6cce
Daniscr99 <danuele.sciarretta@gmail.com>
2021-08-28 19:09:07 +0200

    Update Chome.php

--- Controllo/Chome.php
+++ Controllo/Chome.php
@@ -10,39 +10,50 @@ class Chome
     {
         $vh= Gpreleva::getIstanza('Vhome');     
         $ur= Gpreleva::getIstanza('Clogin'); 
-        $vl= Gpreleva::getIstanza('VUtente');
-        if($vl->getUser() == "" || $vl->getPwd() == "")
+        $vl= Gpreleva::getIstanza('VUtente'); 
+        $vl->caricaindex();
+        if($vl->getUser() == "" && $vl->getPwd() == "")
         {
-            $vl->caricaindex();  
-        }
-        else
-        {
-            $arr=$ur->autenticazione($vl->getUser(),$vl->getPwd());
-            if($arr !== null )
-            {   
-                if($arr->get_tipo()==1)
-                {
-                    $vl->caricaTemplate('username',$arr->get_username(),'Admin.tpl'); 
-                }
-                else
-                {
-                    $dati=array('username'=>$arr->get_username(),'nome'=>$arr->get_nome(),'cognome'=>$arr->get_cognome(),'cod_fisc'=>$arr->get_cdf());
-                    $vl->caricaTemplate('P',$dati,'Paziente.tpl');
-                }         
-            }
-            elseif($vh->Registrazione()!== null)
+            if($vh->Registrazione())
             {
-                echo 'fico';
-                 $ur->InsUtente();
-            }
-
+                    
+                $ur->InsUtente();
             
-
+            }
+        }    
+        else
+        {   
+            $arr=$ur->autenticazione($vl->getUser(),$vl->getPwd());
+            if($arr->get_tipo()==1)
+            {
+                $vl->caricaTemplate('username',$arr->get_username(),'Admin.tpl'); 
+            }
+            else
+            {
+                $dati=array('username'=>$arr->get_username(),'nome'=>$arr->get_nome(),'cognome'=>$arr->get_cognome(),'cod_fisc'=>$arr->get_cdf());
+                $vl->caricaTemplate('P',$dati,'Paziente.tpl');
+            }         
         }
-
- 
     }
 
+  /*  public function smista() {
+        $view=USingleton::getInstance('VHome');
+        switch ($view->getController()) {
+            case 'registrazione':
+                $CRegistrazione=USingleton::getInstance('CRegistrazione');
+                return $CRegistrazione->smista();
+            case 'ricerca':
+                $CRicerca=USingleton::getInstance('CRicerca');
+                return $CRicerca->smista();
+            case 'ordine':
+                $COrdine=USingleton::getInstance('COrdine');
+                return $COrdine->smista();
+            default:
+                $CRicerca=USingleton::getInstance('CRicerca');
+                return $CRicerca->ultimiArrivi();
+        }
+    }
+    */  
        
 }
 ?>
