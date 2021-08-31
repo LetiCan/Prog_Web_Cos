706db4e1172485ecec3ad331ae331d734a444f93
Daniscr99 <danuele.sciarretta@gmail.com>
2021-08-31 18:50:23 +0200

    Update e9da260e77a56550f6332e79239c5543728f9cd8_0.file.Paziente.tpl.php

--- tmpl/templates_c/e9da260e77a56550f6332e79239c5543728f9cd8_0.file.Paziente.tpl.php
+++ tmpl/templates_c/e9da260e77a56550f6332e79239c5543728f9cd8_0.file.Paziente.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-08-27 18:08:19
+/* Smarty version 3.1.39, created on 2021-08-31 18:39:28
   from 'C:\Users\danue\public_html\Prog_Web_Cos\tmpl\templates\Paziente.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_61290df3f16562_70947201',
+  'unifunc' => 'content_612e5b40e2cf12_97525883',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'e9da260e77a56550f6332e79239c5543728f9cd8' => 
     array (
       0 => 'C:\\Users\\danue\\public_html\\Prog_Web_Cos\\tmpl\\templates\\Paziente.tpl',
-      1 => 1630080288,
+      1 => 1630421703,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_61290df3f16562_70947201 (Smarty_Internal_Template $_smarty_tpl) {
+function content_612e5b40e2cf12_97525883 (Smarty_Internal_Template $_smarty_tpl) {
 ?><head>
  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title> COS </title>
@@ -38,16 +38,20 @@ function content_61290df3f16562_70947201 (Smarty_Internal_Template $_smarty_tpl)
   <?php echo '<script'; ?>
  src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"><?php echo '</script'; ?>
 >
+  <?php echo '<script'; ?>
+ src="funzioniJs/funz.js"><?php echo '</script'; ?>
+>
 </head>
 
 <body  class="container-fluid">
 <div id="header">
-	<nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-center fixed-top" >
+	<nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-center " >
 		<a class="navbar-brand"><img src="img/Senza titolo-1.png" width="80px" heigth="25px" ></a>
 		<a class="nav-link" href="#infopa" data-toggle="collapse" style="color: white;"> <?php echo $_smarty_tpl->tpl_vars['P']->value['username'];?>
 </a>
 	</nav>
 </div>
+
 
 <div  class="container-fluid" id="paz">
 
@@ -55,16 +59,19 @@ function content_61290df3f16562_70947201 (Smarty_Internal_Template $_smarty_tpl)
 	<br><br>
 	<table width='100%' >
 	<tr>
-	<td><button id="storico" type="button" class="btn btn-primary">Storico delle prenotazioni</button></td>
-	<td><button id="richiestaP" type="button" class="btn btn-primary" >Richiesta Passaporto</button></td>
-	<td><button id="PrenT" type="button" class="btn btn-primary" onclick="">Prenotazione Tampone</button></td>
-    <td><button id="caricaD" type="button" class="btn btn-primary" onclick="">Caricamento dosi</button></td>
+	<td><button id="storico"  class="btn btn-primary" onclick="fun1()">Storico delle prenotazioni</button></td>
+	<td><button id="pre"  class="btn btn-primary" onclick="" >Prenotazione Tampone</button></td>
+	<td><button id="pass"  class="btn btn-primary" onclick="">Richiesta Passaporto</button></td>
+    <td><button id="dosi"  class="btn btn-primary" onclick="">Caricamento dosi</button></td>
 	</tr>
 	</table>
+ 
+<br>
 </div>
 
 <!-- informazioini del pazinete -->
 <div class="collapse" id='infopa'>
+    <p><h3><b>Anagrafica</b></h3></p>
     <table>
         <tr>
             <td>
@@ -97,6 +104,9 @@ function content_61290df3f16562_70947201 (Smarty_Internal_Template $_smarty_tpl)
 </div>
 
 <br>
-<a href="index.php">home</a>
-</body><?php }
+<form method="POST" action="index.php">
+    <input type="submit" name="lgo" value="Logout"></input>
+</form>
+</body>
+<?php }
 }
