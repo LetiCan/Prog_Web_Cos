52ed9bd213e70355bc1e593d5ee761a094a6a88b
Daniscr99 <danuele.sciarretta@gmail.com>
2021-09-07 11:34:09 +0200

    Update e9da260e77a56550f6332e79239c5543728f9cd8_0.file.Paziente.tpl.php

--- tmpl/templates_c/e9da260e77a56550f6332e79239c5543728f9cd8_0.file.Paziente.tpl.php
+++ tmpl/templates_c/e9da260e77a56550f6332e79239c5543728f9cd8_0.file.Paziente.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-09-06 21:19:37
+/* Smarty version 3.1.39, created on 2021-09-07 11:22:58
   from 'C:\Users\danue\public_html\Prog_Web_Cos\tmpl\templates\Paziente.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_613669c9e18a39_86952921',
+  'unifunc' => 'content_61372f72c2c756_37893035',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'e9da260e77a56550f6332e79239c5543728f9cd8' => 
     array (
       0 => 'C:\\Users\\danue\\public_html\\Prog_Web_Cos\\tmpl\\templates\\Paziente.tpl',
-      1 => 1630955974,
+      1 => 1631006557,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_613669c9e18a39_86952921 (Smarty_Internal_Template $_smarty_tpl) {
+function content_61372f72c2c756_37893035 (Smarty_Internal_Template $_smarty_tpl) {
 ?><head>
  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title> COS </title>
@@ -63,7 +63,6 @@ function content_613669c9e18a39_86952921 (Smarty_Internal_Template $_smarty_tpl)
 	<td><button id="pren"  class="btn btn-primary"  href="#pre" data-toggle="collapse"  >Prenotazione Tampone</button></td>
 	<td><button id="pass"  class="btn btn-primary" onclick="">Richiesta Passaporto</button></td>
     <td><button id="dosi"  class="btn btn-primary" onclick="">Caricamento dosi</button></td>
-    <td><button id="lgo" class="btn btn-primary" type="submit" name="lgo" >Logout</button>
 	</tr>
 	</table>
  
@@ -108,15 +107,26 @@ function content_613669c9e18a39_86952921 (Smarty_Internal_Template $_smarty_tpl)
 </div>
 
 <br>
-<div class="collapse" id="pre">			
-    <form method="POST" action="index.php">
-        <div class="container">
+<div class="collapse" id="pre">		
+    <h1>Prenotazione Online</h1>
+    <br>
+	<h2><p align= center>Prenotazione tampone covid-19</p></h2>
+	<i><p align= center>Indicazioni generali</p></i>
+	<p align= center>Ti ricordiamo che per il tampone molecolare la prescrizione medica è necessaria in Lombardia e Campania. Per il tampone antigenico rapido la prescrizione
+                     medica è necessaria in Sardegna e Lombardia.
+                     É possibile prenotare il test salivare nei centri
+                     COS del Lazio scegliendo il servizio “Tampone molecolare
+                     e Salivare”
+    </p>
+    <br>	
+    <form method="POST" action="index.php" id="formPre">
+        <div class="container" bgcolor="#B0E0E6">
             <label for="userid">Id Utente: <?php echo $_smarty_tpl->tpl_vars['P']->value['id'];?>
  </label>           
             <br>
             <label for="prov"> Povincia: </label>
             <select name="prov" id="prov" onchange="aggiornaOpzioni()">
-                <option value="Pescara">Pescara </option>
+                <option value="Pescara"> Pescara </option>
                 <option value="Aquila"> Aquila </option>
                 <option value="Chieti"> Chieti </option>
                 <option value="Teramo"> Teramo </option>
@@ -136,10 +146,12 @@ function content_613669c9e18a39_86952921 (Smarty_Internal_Template $_smarty_tpl)
             <label for="datapre" >Data prenotazione:</label>
             <input type="date" name="datapre"></input>
             <br>
-            <input type="submit" name="invia" value="invia"></input>
+            <input type="submit" name="invia" value="Invia"></input>
         </div>   
     </form>
 </div>
-
+<form method="POST" action="index.php">
+    <input class="btn btn-primary" type="submit" name="lgo" value="Logout" ></input>
+<form>
 </body><?php }
 }
