f73dc4f4336768d199fb0b4db1c8999eefda9e75
Daniscr99 <danuele.sciarretta@gmail.com>
2021-09-07 11:34:06 +0200

    Update e4611739cb719c455aec57b88a89cab36decb835_0.file.index.tpl.php

--- tmpl/templates_c/e4611739cb719c455aec57b88a89cab36decb835_0.file.index.tpl.php
+++ tmpl/templates_c/e4611739cb719c455aec57b88a89cab36decb835_0.file.index.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-09-06 09:21:02
+/* Smarty version 3.1.39, created on 2021-09-07 11:31:26
   from 'C:\Users\danue\public_html\Prog_Web_Cos\tmpl\templates\index.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_6135c15e175de7_06435195',
+  'unifunc' => 'content_6137316eb87924_88083662',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'e4611739cb719c455aec57b88a89cab36decb835' => 
     array (
       0 => 'C:\\Users\\danue\\public_html\\Prog_Web_Cos\\tmpl\\templates\\index.tpl',
-      1 => 1630912855,
+      1 => 1631007055,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_6135c15e175de7_06435195 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6137316eb87924_88083662 (Smarty_Internal_Template $_smarty_tpl) {
 ?><html>
 <head>
  <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -162,11 +162,6 @@ function content_6135c15e175de7_06435195 (Smarty_Internal_Template $_smarty_tpl)
 			</div>
 	</div>   
 </body>
-	<footer>   
-		<div  id="info">
-			<p>@copyright by..... </p>
-		<div>       
-	</footer>
 	<?php echo '<script'; ?>
 >
 		// Get the modal
