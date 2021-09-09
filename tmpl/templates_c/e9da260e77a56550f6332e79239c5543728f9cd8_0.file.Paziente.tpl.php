ddc2fb86cfa86771b2b8b1d1e2447a2827cb0466
Daniscr99 <danuele.sciarretta@gmail.com>
2021-09-09 09:54:49 +0200

    Update e9da260e77a56550f6332e79239c5543728f9cd8_0.file.Paziente.tpl.php

--- tmpl/templates_c/e9da260e77a56550f6332e79239c5543728f9cd8_0.file.Paziente.tpl.php
+++ tmpl/templates_c/e9da260e77a56550f6332e79239c5543728f9cd8_0.file.Paziente.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-09-07 12:20:47
+/* Smarty version 3.1.39, created on 2021-09-09 09:06:55
   from 'C:\Users\danue\public_html\Prog_Web_Cos\tmpl\templates\Paziente.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_61373cffd7fbf8_34649480',
+  'unifunc' => 'content_6139b28f68cc71_92482384',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'e9da260e77a56550f6332e79239c5543728f9cd8' => 
     array (
       0 => 'C:\\Users\\danue\\public_html\\Prog_Web_Cos\\tmpl\\templates\\Paziente.tpl',
-      1 => 1631010031,
+      1 => 1631171209,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_61373cffd7fbf8_34649480 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6139b28f68cc71_92482384 (Smarty_Internal_Template $_smarty_tpl) {
 ?><head>
  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title> COS </title>
@@ -60,7 +60,7 @@ function content_61373cffd7fbf8_34649480 (Smarty_Internal_Template $_smarty_tpl)
 	<table width='100%' >
 	<tr>
 	<td><button id="storico"  class="btn btn-primary" onclick="fun1()">Storico delle prenotazioni</button></td>
-	<td><button id="pren"  class="btn btn-primary"  href="#pre" data-toggle="collapse"  >Prenotazione Tampone</button></td>
+	<td><button id="pren"  class="btn btn-primary"  onclick="fun1()">Prenotazione Tampone</button></td>
 	<td><button id="pass"  class="btn btn-primary" onclick="">Richiesta Passaporto</button></td>
     <td><button id="dosi"  class="btn btn-primary" onclick="">Caricamento dosi</button></td>
 	</tr>
@@ -76,6 +76,15 @@ function content_61373cffd7fbf8_34649480 (Smarty_Internal_Template $_smarty_tpl)
 <div class="collapse" id='infopa'>
     <p><h3><b>Anagrafica</b></h3></p>
     <table>
+        <tr>
+            <td>
+                <label>Id Utente:</label>           
+            </td>
+            <td>
+                <?php echo $_smarty_tpl->tpl_vars['P']->value['id'];?>
+
+            </td>
+        </tr>
         <tr>
             <td>
                 <label>Nome:</label>  
@@ -107,49 +116,7 @@ function content_61373cffd7fbf8_34649480 (Smarty_Internal_Template $_smarty_tpl)
 </div>
 
 <br>
-<div class="collapse" id="pre">		
-    <h1>Prenotazione Online</h1>
-    <br>
-	<h2><p align= center>Prenotazione tampone covid-19</p></h2>
-	<i><p align= center>Indicazioni generali</p></i>
-	<p align= center>Ti ricordiamo che per il tampone molecolare la prescrizione medica è necessaria in Lombardia e Campania. Per il tampone antigenico rapido la prescrizione
-                     medica è necessaria in Sardegna e Lombardia.
-                     É possibile prenotare il test salivare nei centri
-                     COS del Lazio scegliendo il servizio “Tampone molecolare
-                     e Salivare”
-    </p>
-    <br>	
-    <form method="POST" action="index.php" id="formPre">
-        <div class="container" bgcolor="#B0E0E6">
-            <label for="userid">Id Utente: <?php echo $_smarty_tpl->tpl_vars['P']->value['id'];?>
- </label>           
-            <br>
-            <label for="prov"> Povincia: </label>
-            <select name="prov" id="prov" onchange="aggiornaOpzioni()">
-                <option value="Pescara"> Pescara </option>
-                <option value="Aquila"> Aquila </option>
-                <option value="Chieti"> Chieti </option>
-                <option value="Teramo"> Teramo </option>
-            </select>
-            <br>
-            <label for="lab"> Laboratori: </label>
-            <select name="lab" id="lab">
 
-            </select>
-            <br>
-            <label for="tipotmp" >Tipo Tampone: </label>
-            <select name="tamp" id="tamp">
-                <option value="testrapido">Tampone Rapido</option>
-                <option value="sierologico">Tampone Sierologico</option>
-            </select>
-            <br>
-            <label for="datapre" >Data prenotazione:</label>
-            <input type="date" name="datapre"></input>
-            <br>
-            <input type="submit" name="invia" value="Invia"></input>
-        </div>   
-    </form>
-</div>
 <form method="POST" action="index.php" id="formLog">
     <input class="btn btn-link" type="submit" name="lgo" value="Logout" ></input>
 <form>
