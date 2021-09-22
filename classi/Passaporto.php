738e859d83c4935d1b52768d34434e0ee755a1eb
Daniscr99 <danuele.sciarretta@gmail.com>
2021-09-22 11:54:53 +0200

    Update Passaporto.php

--- classi/Passaporto.php
+++ classi/Passaporto.php
@@ -1,16 +1,4 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of Passaporto
- *
- * @author danue
- */
 class Passaporto 
 {
    private $cod_passaporto;
