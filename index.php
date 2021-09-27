e3d41c7d0045eab88896a518c18c45edda3d9ab7
Daniscr99 <danuele.sciarretta@gmail.com>
2021-09-27 11:18:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <?php 
- require_once 'View/VUtente.php';
- require_once 'View/Vhome.php';
- require_once 'View/VRegistrazione.php';
+ //require_once 'View/VUtente.php';
+ //require_once 'View/Vhome.php';
+ //require_once 'View/VRegistrazione.php';
  require_once 'Foundation/Gestione/Gpreleva.php';
- require_once 'Controllo/Clogin.php';
+ //require_once 'Controllo/Clogin.php';
  require_once 'Foundation/Gestione/Gsessione.php';
  require_once 'Controllo/Chome.php';
  $ses = Gpreleva::getIstanza('Gsessione');
  $cix = Gpreleva::getIstanza('Chome');
- $cix->imp();
- $vl= Gpreleva::getIstanza('VUtente');
- $ur= Gpreleva::getIstanza('Clogin'); 
- 
-  
+ $cix->imp();  
 ?>
