5796a4e72839bcc9d094eff8008bc9fcef051946
Daniscr99 <danuele.sciarretta@gmail.com>
2021-09-27 11:18:16 +0200

    Update FPassaporto.php

--- Foundation/FPassaporto.php
+++ Foundation/FPassaporto.php
@@ -5,7 +5,7 @@ class FPassaporto extends Fconndb
     public function __construct()
     {
         $this->tabella='passaporto';
-        $this->chiave='cod_pass';
+        $this->chiave='cod_passaporto';
         $this->classe='Passaporto';
         $this->autoincremento=true;
         Gpreleva::getIstanza('Fconndb');
