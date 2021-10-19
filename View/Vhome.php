9814823693801232d218e87a9944516a403d7222
Daniscr99 <danuele.sciarretta@gmail.com>
2021-10-19 14:35:05 +0200

    Update Vhome.php

--- View/Vhome.php
+++ View/Vhome.php
@@ -1,23 +1,30 @@
 <?php
 require_once 'View.php';
+require_once 'impo/Utility.php';
+require_once 'Foundation/Gestione/Gpreleva.php';
 class Vhome extends View
 {
  
     public function DatiReg()
     {
-        if(isset($_POST['nome'],$_POST['cognome'],$_POST['datanascita'],$_POST['ldn'],$_POST['cdf'],$_POST['sesso'],$_POST['username'],$_POST['pwd']))
+        if(isset($_POST['nome'],$_POST['cognome'],$_POST['datanascita'],$_POST['ldn'],$_POST['prov'],$_POST['cdf'],$_POST['sesso'],$_POST['username'],$_POST['pwd']))
         {
-            $d=array('nome'=>$_POST['nome'],
-            'cognome'=>$_POST['cognome'],
-            'datanascita'=>$_POST['datanascita'],
-            'ldn'=>$_POST['ldn'],
-            'cdf'=>$_POST['cdf'],
-            'sesso'=>$_POST['sesso'],
-            'username'=>$_POST['username'],
-            'pwd'=>$_POST['pwd']);       
+            $c=Gpreleva::getIstanza('Utility');
+            $ldn=array('com'=>$_POST['ldn'],'pro'=>$_POST['prov']);
+            $cdf_cal=$c->Check_cdf($_POST['nome'],$_POST['cognome'],$_POST['datanascita'],$ldn,$_POST['sesso']);
+            if(strcmp($_POST['cdf'],$cdf_calc) == 0)
+            {
+                $d=array('nome'=>$_POST['nome'],
+                'cognome'=>$_POST['cognome'],
+                'datanascita'=>$_POST['datanascita'],
+                'ldn'=>$_POST['ldn'],
+                'cdf'=>$_POST['cdf'],
+                'sesso'=>$_POST['sesso'],
+                'username'=>$_POST['username'],
+                'pwd'=>$_POST['pwd']);   
+            }      
         }
         return $d;
-           
     }
 }
 ?>
