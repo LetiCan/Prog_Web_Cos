f65cf8ba43f1c492ab87a129b271fda2d2e888b8
Daniscr99 <danuele.sciarretta@gmail.com>
2021-10-21 10:30:35 +0200

    Update Vhome.php

--- View/Vhome.php
+++ View/Vhome.php
@@ -11,14 +11,15 @@ class Vhome extends View
         {
             $c=Gpreleva::getIstanza('Utility');
             $ldn=array('com'=>$_POST['ldn'],'pro'=>$_POST['prov']);
+            $cdf_ins=strtoupper($_POST['cdf']);
             $cdf_cal=$c->Check_cdf($_POST['nome'],$_POST['cognome'],$_POST['datanascita'],$ldn,$_POST['sesso']);
-            if(strcmp($_POST['cdf'],$cdf_calc) == 0)
+            if(strcmp($cdf_ins,$cdf_cal) == 0)
             {
                 $d=array('nome'=>$_POST['nome'],
                 'cognome'=>$_POST['cognome'],
                 'datanascita'=>$_POST['datanascita'],
                 'ldn'=>$_POST['ldn'],
-                'cdf'=>$_POST['cdf'],
+                'cdf'=>$cdf_ins,
                 'sesso'=>$_POST['sesso'],
                 'username'=>$_POST['username'],
                 'pwd'=>$_POST['pwd']);   
